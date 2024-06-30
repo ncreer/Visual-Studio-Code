c7cb3845c86823e26c6e4a421d74a5ed5c0967d6
ncreer <natekc@byui.edu>
2024-06-29 19:38:48 -0600

    updated

--- cse110/cart.py
+++ cse110/cart.py
@@ -9,11 +9,31 @@ while program_running == True:
     print("3. Remove item")
     print("4. Compute total")
     print("5. Quit")
-    action = input("Please enter an action: ")
-    if action == "1":
+    action = int(input("Please enter an action: "))
+    if action == 1:
         item = input("What item would you like to add? ")
-        cart.append(item)
-    if action == "2":
+        price = float(input(f"What is the price of '{item}'? "))
+        cart.append((item, price))
+        print(f"'{item}' has been added to the cart.")
+        print("")
+    if action == 2:
         print("The contents of the shopping cart are:")
-        for item in cart:
-            print(item)
+        for index, (item, price) in enumerate(cart):
+            print(f"{index + 1}. {item} - ${price:.2f}")
+        print("")
+    if action == 3:
+        remove = int(input("Which item would you like to remove? "))
+        try:
+            removed_item = cart.pop(remove - 1)
+            print("Item removed.")
+            print("")
+        except IndexError:
+            print("Sorry, that is not a valid item number.")
+            print("")
+    if action == 4:
+        total = sum(price for item, price in cart)
+        print(f"The total price of the items in the shopping cart is ${total:.2f}")
+        print("")
+    if action == 5:
+        program_running = False
+print("Thank you. Goodbye.")
